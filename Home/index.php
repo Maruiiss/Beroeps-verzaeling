986bc7a29435f68a9b7f2c2b2c654d372c2e60be
John <95253777+Maruiiss@users.noreply.github.com>
2023-10-05 12:22:12 +0200

    verderaan gewerkt met het theme

--- Home/index.php
+++ Home/index.php
@@ -6,27 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script src="script.js" defer></script>
-    <title>Verzamelaars Vereniging</title>
+    <title class="btext">Alcohol Lovers Club</title>
 </head>
 
 <body>
     <header>
-        <h1>Verzamelaars Vereniging</h1>
+        <h1 class="btext" id="title">Alcohol Lovers Club</h1>
         <nav>
             <ul>
-                <li><a href="#startpagina">Startpagina</a></li>
-                <li><a href="#verzameling">Verzameling</a></li>
-                <li><a href="#bestellingen">Bestellingen</a></li>
+                <li><a href="#startpagina"> <button class="navText">Home</button> </a></li>
+                <li><a href="#verzameling"> <button class="navText">Soorten Drank</button> </a></li>
+                <li><a href="#bestellingen"> <button class="navText">Bestellingen</button> </a></li>
             </ul>
         </nav>
     </header>
     <main>
         <section id="startpagina">
-            <h2>Welkom bij de Verzamelaars Vereniging</h2>
-            <p>Ontdek onze geweldige collecties en bestel je favoriete items.</p>
+            <h2>Welkom bij de Alcohol Lovers Club</h2>
+            <p>Ontdek de fascinerende wereld van alcoholische dranken en bestel je favoriete drankjes.</p>
         </section>
         <section id="verzameling">
-            <h2>Onze Verzameling</h2>
+            <h2>Onze Dranken</h2>
             <div id="item-list">
 
             </div>
@@ -39,7 +39,9 @@
         </section>
     </main>
     <footer>
-        <?php echo date("l jS \of F Y h:i:s A"); ?>
+        <p class="btext">Lovers Club</p>
+        <?php echo date("jS \of F Y h:i:s A"); ?>
+
     </footer>
 </body>
 
--- Home/script.js
+++ Home/script.js
@@ -1,21 +1,19 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-
     const itemContainer = document.getElementById("item-list");
 
     for (let i = 1; i <= 8; i++) {
         const itemCard = document.createElement("div");
         itemCard.classList.add("item-card");
         itemCard.innerHTML = `
-            <h3>Verzamelitem ${i}</h3>
-            <p>Beschrijving van verzamelitem ${i}</p>
+            <h3>Alcoholische Drank ${i}</h3>
+            <p>Beschrijving van drank ${i}</p>
             <button onclick="bestelItem(${i})">Bestel</button>
         `;
         itemContainer.appendChild(itemCard);
     }
 
-    // Voorbeeldfunctie om een item te bestellen
+
     function bestelItem(itemId) {
-        alert(`Je hebt verzamelitem ${itemId} besteld!`);
+        alert(`Je hebt alcoholische drank ${itemId} besteld!`);
     }
 });
